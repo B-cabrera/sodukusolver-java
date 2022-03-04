88ea25502ae4a3ebacca196bddda57cad058b20e
B-cabrera <brendenc29@gmail.com>
2022-03-04 08:56:39 -0500

    Added required packages and configs to begin making UI

--- UI.java
+++ UI.java
@@ -1,6 +1,10 @@
-class UI {
+public class UI {
 
     /* This will be the file that I will be making
     the user interface in.
     */
+
+    public static void main(String[] args) {
+        System.out.println ("Makiing sure everything works!");
+    }
 }

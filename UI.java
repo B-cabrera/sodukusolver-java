0833612e0ae02a639e17adac7f1076a5a4d56ae6
B-cabrera <brendenc29@gmail.com>
2022-03-07 08:04:08 -0500

    Made starter window, set up files to successfully run UI

--- UI.java
+++ UI.java
@@ -1,10 +1,27 @@
-public class UI {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UI extends Application{
 
     /* This will be the file that I will be making
     the user interface in.
     */
 
     public static void main(String[] args) {
-        System.out.println ("Makiing sure everything works!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene firstScene = new Scene(root, 500,500);
+
+        stage.setScene(firstScene);
+        stage.setTitle("Testing 1 2 3");
+        stage.show();
+
+        
     }
 }

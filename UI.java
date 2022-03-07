9b719714a1fbdb6418a110766abbff923fbe4575
B-cabrera <brendenc29@gmail.com>
2022-03-07 11:22:12 -0500

    Set text to top middle that says 'Enter a Sudoku Board'

--- UI.java
+++ UI.java
@@ -1,6 +1,12 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class UI extends Application{
@@ -15,11 +21,21 @@ public class UI extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
+        Text instructions = new Text();
+        instructions.setText("Enter a Sudoku Board");
+        instructions.setX(150);
+        instructions.setY(20);
+        instructions.setFont(new Font(30));
+
+
+        VBox root = new VBox(instructions);
+        root.setAlignment(Pos.TOP_CENTER);
+        instructions.setTextAlignment(TextAlignment.CENTER);
         Scene firstScene = new Scene(root, 500,500);
 
+        
         stage.setScene(firstScene);
-        stage.setTitle("Testing 1 2 3");
+        stage.setTitle("Sudoku Solver");
         stage.show();
 
         
